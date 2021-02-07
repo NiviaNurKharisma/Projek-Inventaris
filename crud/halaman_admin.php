383cf6bcbcb6e76350bab3e6b8747d57ccd0fcca
Nivia Nur Kharisma <53246336+NiviaNurKharisma@users.noreply.github.com>
2021-02-07 13:05:13 +0700

    Add files via upload

---
+++ crud/halaman_admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Admin</title>
+</head>
+<body>
+	<?php 
+	session_start();
+
+	// cek apakah yang mengakses halaman ini sudah login
+	if($_SESSION['level']==""){
+		header("location:index.php?pesan=gagal");
+	}
+
+	?>
+	<h1>Hello Admin!</h1>
+
+	<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?></b>.</p>
+	<a href="logout.php">LOGOUT</a>
+
+	<br/>
+	<br/>
+</html>
